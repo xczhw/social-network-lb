cde32ce907f51778feada7fb004cf9014a097078
xczhw <xczhw@outlook.com>
2024-03-19 03:32:40 -0400

    清理了cache. 添加了解压output的方法

--- drawer/main.py
+++
@@ -1,8 +0,0 @@
-from utils import *
-import pathlib
-
-path = pathlib.Path('../output/round-robin/RPS_-1/0/compose-post-service/')
-
-unzip(path,'10.244.1.164.zip')
-
-
--- drawer/utils.py
+++
@@ -1,5 +0,0 @@
-import zipfile
-
-def unzip(path, filename):
-    with zipfile.ZipFile(path / filename, 'r') as zip_ref:
-        zip_ref.extractall(path / filename.strip('.zip'))
