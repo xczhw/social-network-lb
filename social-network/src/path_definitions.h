c4f3a469a40ff447fda4897d825e7c1c93cf41a6
xczhw <xczhw@outlook.com>
2025-02-26 19:17:30 -0700

    reset social  Network

--- social-network/src/path_definitions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace paths {
-    const std::string ROOTPATH = "/share";
-    const std::string DATAPATH = ROOTPATH + "/data/";
-    const std::string LOGPATH = ROOTPATH + "/logs/";
-}
